7b98690c6351bc5f97ae4d2f2f1914065ec02f5c
Artemchuvashov2332 <59090449+Artemchuvashov2332@users.noreply.github.com>
2020-06-02 22:25:32 +0300

    Add files via upload

---
+++ mp2-lab6-tables_laba daba/lab2/labTables/exception_example.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <typeinfo>
+#include <stdio.h> 
+#include <string>
+#include <time.h>
+#include "Table.h"
+#include "HashTable.h"
+
+using namespace std;
+class PositiveNumber
+{
+    int positiveNumber;
+public:
+    PositiveNumber(int num) :positiveNumber(num)
+    {
+        if (num < 0)
+            throw exception("negative number");
+    }
+};
+int main()
+{
+    /*
+    //string a = "1";
+    int a = -1;
+    auto& ti1 = typeid(a);
+    cout << ti1.name() << endl;
+    // runtime_error
+    try
+    {
+        PositiveNumber n(a);
+    }
+    catch (exception &e)
+    {
+        cerr << "Caught " << e.what() << endl;
+        cerr << "Type " << typeid(e).name() << endl;
+    };*/
+
+    /*
+    Table<int, int> test1(3);
+    test1.Add(0, 3);
+    test1.Add(1, 1);
+    test1.Add(2, 2);
+    for (int i = 0; i < 3; i++)
+        cout << test1.Find(i) << " ";
+    SortTable<int, int> sortTest1(test1);
+    sortTest1.Delete(1);
+    try { sortTest1.Find(1); }
+    catch (...)
+    {
+        cerr << "\nCaught exeption\n";
+    }
+    cout << sortTest1.Find(0) << endl;*/
+
+    HashTable<string, int> TestHashFunc(10);
+
+    TestHashFunc.Add("first", 11);
+    TestHashFunc.Add("second", 22);
+
+    cout << TestHashFunc.Find("first") << endl;
+}
+/* Output:
+Caught bad locale name
+Type class std::runtime_error
+*/
